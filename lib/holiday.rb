05501f3f538be70d9ffcaae7d5d02466087f75b1
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-08 16:07:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   
-  return holiday_hash[:winter][:memorial_day]
+  return holiday_hash[:winter]
 
 end
 
